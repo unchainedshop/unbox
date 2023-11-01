ee1fd4ebdd7c25e5160a2150db05c4212c10a719
Pascal Kaufmann <pascal@unchained.shop>
2023-11-01 13:43:03 +0100

    Expose helios rpc

--- make-bundle.sh
+++ make-bundle.sh
@@ -1,4 +1,5 @@
 docker build -t registry.ucc.dev/unchained/kitchensink:box -f ./engine/Dockerfile ./engine
+docker build -t registry.ucc.dev/unchained/heliox:box -f ./helios/Dockerfile ./helios
 # docker build -f ./storefront/Dockerfile ./storefront registry.ucc.dev/unchained/storefront:box
 
 docker pull -q registry.ucc.dev/unchained/storefront:box
@@ -9,11 +10,11 @@ docker pull -q jnovack/autossh:2.0.1
 
 docker image save -o ./bundle/kitchensink.tar registry.ucc.dev/unchained/kitchensink:box
 docker image save -o ./bundle/storefront.tar registry.ucc.dev/unchained/storefront:box
+docker image save -o ./bundle/helios.tar registry.ucc.dev/unchained/heliox:box
 docker image save -o ./bundle/mongo.tar mongo:4.4.18-focal
 docker image save -o ./bundle/wstunnel.tar ghcr.io/erebe/wstunnel:v7.8.2
 docker image save -o ./bundle/traefik.tar traefik:v2.9
 docker image save -o ./bundle/autossh.tar jnovack/autossh:2.0.1
-docker image save -o ./bundle/helios.tar helios
 
 cp ./init-bundle.sh ./bundle/init-bundle.sh
 cp ./shop-stack.yml ./bundle/shop-stack.yml
